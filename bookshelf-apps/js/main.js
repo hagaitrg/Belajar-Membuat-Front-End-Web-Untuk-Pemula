f4ca96a6ab5972d28382547a5e3d0ba0ea451cc7
hagaitrg <disahagait@gmail.com>
2021-07-24 02:50:49 +0700

    initial commit for submission

---
+++ bookshelf-apps/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const inputBook = document.getElementById("inputBook");
+
+  submitForm.addEventListener("submit", function (event) {});
+});
